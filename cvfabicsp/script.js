4f92e2b4f7bf65283472d81584e7e478be5d5680
Peter Fabics <fapqabt@gmail.com>
2017-01-08 12:10:10 +0100

    my cv

---
+++ cvfabicsp/script.js
@@ -0,0 +1,22 @@
+window.onload = $(function(){
+  // console.log("hi");
+  $(".tile").not(".contact").fadeOut();
+});
+
+$(function() {
+		var selectedClass = "";
+		$(".headline").click(function(){
+      var pheaders = $(".headline");
+  		selectedClass = $(this).attr("data-rel");
+      if ($(".highlighted").length > 0) {
+        $(".highlighted").removeClass( "highlighted" );
+        // $(".highlighted")[0].className = "headline";
+      }
+      this.className += " highlighted";
+  		$(".tile").not("."+selectedClass).fadeOut();
+      setTimeout(function() {
+        $("."+selectedClass).fadeIn();
+        // $("#portfolio").fadeTo(500, 1);
+      }, 500);
+	});
+});
